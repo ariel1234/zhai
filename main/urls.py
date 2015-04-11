4633d0a819d87cbcec1dc12f3b0806d6a10920c3
林浩特 <htlin@x-one.cc>
2015-04-11 14:31:57 +0800

    实现首页和加载页面的框架。

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
 
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^db/$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.loading, name='loading'),
     url(r'^home/', views.home, name='home'),
     url(r'^loading/', views.loading, name='loading'),
     url(r'^house/(?P<pk>[0-9]+)', views.DetailView.as_view(), name="house_detail")
